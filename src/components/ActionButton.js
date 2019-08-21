457412266bd2082d43a43ab08f2239c9a44d25d0
Pah Ferreira <pah.dsf@gmail.com>
2019-08-21 19:07:44 -0300

    Button Color

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Text, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/dist/FontAwesome5';
+import Icon from 'react-native-vector-icons/dist/MaterialIcons';
 import styles from './styles/ActionButtonStyle';
 
 const ActionButton = props => {
   return (
-    <TouchableOpacity style={styles.container}>
-      <Text>{props.name}</Text>
-      <Icon name={props.icon} size={16} color="black" />
+    <TouchableOpacity
+      style={[styles.container, { backgroundColor: props.color }]}>
+      <Text style={styles.text}>{props.name}</Text>
+      <Icon name={props.icon} size={14} color="white" />
     </TouchableOpacity>
   );
 };
