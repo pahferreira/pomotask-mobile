d2032f55dfd5bd02162289ad9abcd50514375121
Pah Ferreira <pah.dsf@gmail.com>
2019-08-27 17:04:27 -0300

    clock fix margin bot

--- src/components/styles/ClockStyle.js
+++ src/components/styles/ClockStyle.js
@@ -1,9 +1,12 @@
 import { StyleSheet } from 'react-native';
+import { Colors } from '../../theme/Theme';
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 80,
     fontFamily: 'SourceCodePro',
+    marginBottom: 20,
+    color: Colors.BLACK,
   },
 });
 
