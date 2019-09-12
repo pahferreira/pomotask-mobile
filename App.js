60f68f58cb1cb357cf8aaa06befad5708676f656
Pah Ferreira <pah.dsf@gmail.com>
2019-09-12 15:49:37 -0300

    implementing context API

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, View, StatusBar } from 'react-native';
 import AppNavigation from './src/navigation/AppNavigation';
 import { MenuProvider } from 'react-native-popup-menu';
+import Context, { reducer, initialState } from './src/context/Context';
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <MenuProvider backHandler={true}>
-      <View style={styles.container}>
-        <StatusBar hidden={true} />
-        <AppNavigation />
-      </View>
-    </MenuProvider>
+    <Context.Provider value={{ state, dispatch }}>
+      <MenuProvider backHandler={true}>
+        <View style={styles.container}>
+          <StatusBar hidden={true} />
+          <AppNavigation />
+        </View>
+      </MenuProvider>
+    </Context.Provider>
   );
 };
 
